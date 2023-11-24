cce416210060654476de372b2558ceb3cc5a06d9
oussama <benslamaoussama1919@gmail.com>
2023-11-24 15:20:40 +0100

    add files

---
+++ admin/src/components/Login.jsx
@@ -0,0 +1,50 @@
+// Login.js
+import React, { useState } from "react";
+import './styles/Login.css'
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    if (email === "" || password === "") {
+      alert("Email and password are mandatory.");
+      return;
+    }
+  
+   
+  };
+  
+  return (
+    <div className="Login">
+        <div className="login-form">
+          <h2>Login</h2>
+          <form>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+              placeholder="Email"
+            />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="password"
+              placeholder="Password"
+            />
+            <input 
+                type="submit" 
+                value="Sign In"
+                onClick={handleLogin}
+                id="signinbtn"
+            />
+          </form>
+        </div>
+      </div>
+    
+  );
+};
+
+export default Login;
---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Navbar = () => {
+  
+  return (
+    <div className="Navbar">
+      <div className="Doctor-nav"> 
+        <ul>
+          
+        </ul>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
