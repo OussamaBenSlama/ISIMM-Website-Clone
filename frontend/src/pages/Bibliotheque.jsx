ac2f6395ac743b8ae51f4bbc08295add3cd5ee25
oussama <benslamaoussama1919@gmail.com>
2024-02-27 23:16:21 +0100

    page biblio

--- frontend/src/pages/Bibliotheque.jsx
+++ frontend/src/pages/Bibliotheque.jsx
@@ -13,9 +13,17 @@ const Biblio = () => {
   return (
     <div>
        <Navbar setMenuVisibility = {setMenuVisibility} isMenuVisible = {isMenuVisible}/>
-       <div className={`home-content ${isMenuVisible ? 'blurred' : ''}`}>
-            <Header/>
-            <Bibliotheque/>
+       <div style={{display:'flex',justifyContent:'center',alignItems:'center',backgroundColor:'rgb(248,249,250)'}} className={`home-content ${isMenuVisible ? 'blurred' : ''}` }>
+            <div style={{width:'60%',paddingTop:'50px'}}>
+              <h3 style={{ color:'#003366', fontSize:'40px' ,marginBottom:'30px'}}>Inscription bibliothèque :</h3>
+              <p style={{lineHeight:'2'}}>
+                Afin de bénéficier des services de prêt des ouvrages, les usagers sont priés de s’inscrire à la bibliothèque dans le portail des bibliothèques universitaires « BIRUNI ».
+              </p>
+              
+              <Header/>
+              <Bibliotheque/>
+            </div>
+
 
         </div>
         {isMenuVisible && <div className={`menu-content show`}><Menu onClose={toggleMenu} /></div>}
