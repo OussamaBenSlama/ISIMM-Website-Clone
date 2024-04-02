699698c494878f19c1315561f99b6898728bc9d6
MahaAzouzi <mahaazouzi45@gmail.com>
2024-04-02 19:47:55 +0100

    email problem

--- backend/projectFolder/settings.py
+++ backend/projectFolder/settings.py
@@ -89,7 +89,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mahaazouzi45@gmail.com'
-EMAIL_HOST_PASSWORD = 'fwabnreejznbngmm'
+EMAIL_HOST_PASSWORD = 'jivw grhr bymz bldx'
 EMAIL_USE_TLS = True
 
 
